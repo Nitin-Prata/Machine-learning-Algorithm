006354ec8d3ca142828d10f9c9bc909baea9fa45
Nitin Pratap Singh <nitinpratap997@gmail.com>
2025-11-03 14:57:20 +0530

    KNN

--- KNN/knn_test.py
+++ KNN/knn_test.py
@@ -1,38 +1,44 @@
+import os
 import numpy as np
 import pandas as pd
 from KNearestNeighbor import KNearestNeighbors
-
-data=pd.read_csv('KNN\Social_Network_Ads.csv')
-
-X=data.iloc[:,2:4].values
-y=data.iloc[:,-1].values
-
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
-
 from sklearn.preprocessing import StandardScaler
-scaler=StandardScaler()
 
-X_train=scaler.fit_transform(X_train)
-X_test=scaler.transform(X_test)
+# Load data from the same directory as this script (robust path)
+BASE_DIR = os.path.dirname(__file__)
+data_path = os.path.join(BASE_DIR, "Social_Network_Ads.csv")
+data = pd.read_csv(data_path)
+
+X = data.iloc[:, 2:4].values
+y = data.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 # An object of KNN
-knn=KNearestNeighbors(k=5)
+knn = KNearestNeighbors(k=5)
+knn.fit(X_train, y_train)
 
-knn.fit(X_train,y_train)
 
 def predict_new():
-    age=int(input("Enter the age"))
-    salary=int(input("Enter the salary"))
-    X_new=np.array([[age],[salary]]).reshape(1,2)
+    age = int(input("Enter the age: "))
+    salary = int(input("Enter the salary: "))
+    X_new = np.array([age, salary]).reshape(1, 2)
 
-    X_new=scaler.transform(X_new)
+    X_new = scaler.transform(X_new)
 
-    result=knn.predict(X_new)
+    result = knn.predict(X_new)
 
-    if result==0:
+    if result[0] == 0:
         print("Will not purchase")
     else:
         print("Will purchase")
 
-predict_new()
+
+if __name__ == "__main__":
+    predict_new()
