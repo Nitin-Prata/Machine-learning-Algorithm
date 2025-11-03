7b70ee16bb82d4620c4e43c0311f8f6a3d49049c
Nitin Pratap Singh <nitinpratap997@gmail.com>
2025-11-03 14:57:51 +0530

    K-Means-clustering

--- K-Means-clustering/app.py
+++ K-Means-clustering/app.py
@@ -1,24 +1,20 @@
-from sklearn.datasets import make_blobs
+import os
 import matplotlib.pyplot as plt
 from kmeans import KMeans
 import pandas as pd
 
-#centroids = [(-5,-5),(5,5),(-2.5,2.5),(2.5,-2.5)]
-#cluster_std = [1,1,1,1]
+# Use a repository-relative path to the example dataset (safer than absolute path)
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+data_path = os.path.join(BASE_DIR, "DataSets", "student_clustering.csv")
 
-#X,y = make_blobs(n_samples=100,cluster_std=cluster_std,centers=centroids,n_features=2,random_state=2)
+df = pd.read_csv(data_path)
+X = df.iloc[:, :].values
 
-#plt.scatter(X[:,0],X[:,1])
-
-df = pd.read_csv(r'C:\Users\HP\ML-Algorithms\DataSets\student_clustering.csv')
-
-X = df.iloc[:,:].values
-
-km = KMeans(n_clusters=4,max_iter=500)
+km = KMeans(n_clusters=4, max_iter=500)
 y_means = km.fit_predict(X)
 
-plt.scatter(X[y_means == 0,0],X[y_means == 0,1],color='red')
-plt.scatter(X[y_means == 1,0],X[y_means == 1,1],color='blue')
-plt.scatter(X[y_means == 2,0],X[y_means == 2,1],color='green')
-plt.scatter(X[y_means == 3,0],X[y_means == 3,1],color='yellow')
+plt.scatter(X[y_means == 0, 0], X[y_means == 0, 1], color="red")
+plt.scatter(X[y_means == 1, 0], X[y_means == 1, 1], color="blue")
+plt.scatter(X[y_means == 2, 0], X[y_means == 2, 1], color="green")
+plt.scatter(X[y_means == 3, 0], X[y_means == 3, 1], color="yellow")
 plt.show()
